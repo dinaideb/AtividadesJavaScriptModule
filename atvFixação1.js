7dce8893c28034098ff3811e32dcedfdc9240a87
dinaideb <132600485+dinaideb@users.noreply.github.com>
2023-06-26 18:36:54 -0300

    Add files via upload
    atividade de fixação 1 26/06 Tquinta

---
+++ atvFixação1.js
@@ -0,0 +1,52 @@
+// number
+var numInteiro= 10;
+console.log(numInteiro);
+
+var numNegativo = -2;
+console.log(numNegativo);
+
+//string
+var texto = ("dinaide");
+console.log(texto);
+
+var sobrenome = ("Barbosa");
+console.log(sobrenome);
+
+// boolean
+var verdade = true;
+var mentira = false;
+
+console.log("você está feliz?");
+console.log(verdade);
+
+console.log("Você é sempre feliz?");
+console.log(mentira);
+
+console.log('Você estuda?');
+console.log(verdade)
+
+console.log('Nunca faltou?');
+console.log(mentira);
+
+//array
+var nomes = ["Dinaide", "Bianca", "Josilene", "Jackson"];
+
+console.log(nomes);
+
+var marcaCelulares = [ 'Samsung', 'LG', 'Motog', 'Apple', 'Xiaomi' ];
+console.log(marcaCelulares);
+
+// Undefined
+var num1;
+console.log(num1);
+
+var idade;
+console.log(idade);
+
+//Null
+var valor=null;
+console.log(valor);
+
+var quantidade=null;
+console.log(quantidade);
+
